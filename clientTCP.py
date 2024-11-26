5487f175551674c7672662bb5aaa2fc3471663b1
Gustavo Paulino <gustavo.vinicius.paulino@gmail.com>
2024-11-25 21:17:40 -0300

    envia arquivo .txt e conta tempo tcp

--- clientTCP.py
+++ clientTCP.py
@@ -1,22 +1,40 @@
 import socket
+import os
+import time
 
-host = input("Insira o IP do servidor: ")
-port = int(input("Insira a porta do servidor: "))
+host = "127.0.0.1"
+port = 65432
 fileName = input("Insira o nome do arquivo para enviar: ")
+buffer_size = 2048
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((host, port))
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host, port))
 
-    try: 
-        # Reading file and sending data to server 
-        file = open(fileName, "r") 
-        data = file.read() 
-        print(data)
-        while data: 
-            s.send(str(data).encode()) 
-            data = file.read() 
-
-    except IOError: 
-            print('Arquivo inválido') 
-
-print(f"Received {data!r}")
+try:
+    # Lendo o arquivo e enviando os dados ao servidor
+    filesize = os.path.getsize(fileName)
+    s.send(f"{fileName}_{filesize}".encode())  # Enviando nome e tamanho do arquivo
+    response = s.recv(buffer_size).decode()
+    print(f"[+] {response}")
+    if response != "Nome e tamanho do arquivo recebidos":
+        print("[Erro] Não foi possível enviar o arquivo.")
+        s.close()
+        exit()
+        
+    with open(fileName, "rb") as file:  # Abrir em modo binário
+        inicio_tempo = time.time()
+        while chunk := file.read(buffer_size):  # Ler em pedaços
+            s.send(chunk)  # Enviar pedaço ao servidor
+    
+    response = s.recv(buffer_size).decode()
+    if response != "Arquivo recebido com sucesso":
+        print("[+] Erro ao receber arquivo")
+    else:
+        print("[+] Arquivo recebido com sucesso")
+        
+    fim_tempo = time.time()
+    rtt = (fim_tempo - inicio_tempo) * 1000 # x1000 para ser em ms
+    print(f"(RTT: {rtt:.2f} ms)")
+    s.close()
+except IOError:
+        print('Arquivo inválido ou erro ao abrir o arquivo.')
--- serverTCP.py
+++ serverTCP.py
@@ -1,21 +1,31 @@
 import socket
 
-host = input("Insira o IP do servidor: ")
-port = int(input("Insira a porta do servidor: "))
+host = "127.0.0.1"
+port = 65432
+buffer_size = 2048
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((host, port))
     s.listen()
+    print("[*] Aguardando conexão...")
     conn, addr = s.accept()
     with conn:
-        print(f"Connected by {addr}")
-        while True:
-            data = conn.recv(1024).decode()
-            if not data:
-                break
-            
-            fo = open("output.txt", "w+")
+        print(f"[+] Conectado por {addr}")
+        file_info = conn.recv(buffer_size).decode()
+        infos = file_info.split("_")
+        filename = infos[0]
+        filesize = int(infos[1])
+        
+        print("[+] Nome e tamanho do arquivo recebidos do cliente.")
+        conn.send("Nome e tamanho do arquivo recebidos".encode())
+        
+        # Recebendo e salvando o arquivo
+        with open(f"output_{filename}", "wb") as fo:  # Salvar em modo binário
+            total_received = 0
+            while total_received < filesize:
+                data = conn.recv(buffer_size)
+                fo.write(data)
+                total_received += len(data)  # Atualizar o progresso
 
-            fo.write(data)
-            fo.close()
-            
+        conn.send("Arquivo recebido com sucesso".encode())
+        print("[+] Arquivo recebido e salvo com sucesso.")
